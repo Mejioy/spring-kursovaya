64e84c78e4cc39a2fdaccf70b3ac4e6e2d76e2d8
Кокурин Ярослав <yaroslav.kokurin@mail.ru>
2024-11-20 18:20:34 +0300

    Initial commit

---
+++ src/test/java/ru/kafpin/firsttt/FirstttApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kafpin.firsttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
