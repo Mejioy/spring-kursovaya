8809f4d68420fbe4fd036e2a9fbf59e8a8396b6c
Кокурин Ярослав <yaroslav.kokurin@mail.ru>
2024-12-25 05:44:14 +0300

    Migrate mo mssql server

--- src/main/java/ru/kafpin/firsttt/GetUserPassword.java
+++ src/main/java/ru/kafpin/firsttt/GetUserPassword.java
@@ -3,8 +3,8 @@ package ru.kafpin.firsttt;
 public class GetUserPassword {
     public static void main(String[] args) {
 
-        System.out.printf("Пароль для %s - %s%n","user1", new WebSecurityConfig().passwordEncoder().encode("user1"));
-        System.out.printf("Пароль для %s - %s%n","user2", new WebSecurityConfig().passwordEncoder().encode("user2"));
+        System.out.printf("Пароль для %s - %s%n","admin", new WebSecurityConfig().passwordEncoder().encode("user1"));
+        System.out.printf("Пароль для %s - %s%n","employer", new WebSecurityConfig().passwordEncoder().encode("user2"));
         System.out.printf("Пароль для %s - %s%n","user3", new WebSecurityConfig().passwordEncoder().encode("user3"));
 
     }
